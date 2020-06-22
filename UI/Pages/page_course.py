88c862b7a197549b6a7a4e3ded2d837f970226ca
josh <rcxwhiz@gmail.com>
2020-06-22 11:52:45 -0500

    fully moved course page to separate class

--- UI/Pages/page_course.py
+++ UI/Pages/page_course.py
@@ -1,117 +1,248 @@
 from PyQt5 import QtCore, QtWidgets
 
+import Dao
+import UI.dialog_course
 
-def add_course_page(stacked_widget: QtWidgets.QStackedWidget,
-                    filter_action: callable,
-                    show_course_action: callable,
-                    return_action: callable):
-	page = QtWidgets.QWidget()
-	grid_layout_1 = QtWidgets.QGridLayout(page)
-	horizontal_layout_1 = QtWidgets.QHBoxLayout()
-	vertical_layout_1 = QtWidgets.QVBoxLayout()
-	# TODO see if this line has an effect
-	vertical_layout_1.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
 
-	dept_label = QtWidgets.QLabel("Dept", page)
-	vertical_layout_1.addWidget(dept_label)
+class CoursePage:
+	def __init__(self, stacked_widget, return_function, data):
+		self.data = data
+		page = QtWidgets.QWidget()
+		grid_layout_1 = QtWidgets.QGridLayout(page)
+		horizontal_layout_1 = QtWidgets.QHBoxLayout()
+		vertical_layout_1 = QtWidgets.QVBoxLayout()
+		# TODO see if this line has an effect
+		vertical_layout_1.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
 
-	dept_edit = QtWidgets.QLineEdit(page)
-	size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
-	size_policy.setHorizontalStretch(0)
-	size_policy.setVerticalStretch(0)
-	size_policy.setHeightForWidth(dept_edit.sizePolicy().hasHeightForWidth())
-	dept_edit.setSizePolicy(size_policy)
-	vertical_layout_1.addWidget(dept_edit)
-	dept_edit.textChanged.connect(filter_action)
+		dept_label = QtWidgets.QLabel("Dept", page)
+		vertical_layout_1.addWidget(dept_label)
 
-	num_label = QtWidgets.QLabel("Course #", page)
-	vertical_layout_1.addWidget(num_label)
+		self.dept_edit = QtWidgets.QLineEdit(page)
+		size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+		size_policy.setHorizontalStretch(0)
+		size_policy.setVerticalStretch(0)
+		size_policy.setHeightForWidth(self.dept_edit.sizePolicy().hasHeightForWidth())
+		self.dept_edit.setSizePolicy(size_policy)
+		vertical_layout_1.addWidget(self.dept_edit)
+		self.dept_edit.textChanged.connect(self.filter_table)
 
-	num_edit = QtWidgets.QLineEdit(page)
-	num_edit.setSizePolicy(size_policy)
-	vertical_layout_1.addWidget(num_edit)
-	num_edit.textChanged.connect(filter_action)
+		num_label = QtWidgets.QLabel("Course #", page)
+		vertical_layout_1.addWidget(num_label)
 
-	credit_hours_check = QtWidgets.QCheckBox("Credit Hours", page)
-	vertical_layout_1.addWidget(credit_hours_check)
-	credit_hours_check.stateChanged.connect(filter_action)
+		self.num_edit = QtWidgets.QLineEdit(page)
+		self.num_edit.setSizePolicy(size_policy)
+		vertical_layout_1.addWidget(self.num_edit)
+		self.num_edit.textChanged.connect(self.filter_table)
 
-	credit_hour_spinner = QtWidgets.QDoubleSpinBox(page)
-	credit_hour_spinner.setDecimals(1)
-	credit_hour_spinner.setSingleStep(0.5)
-	vertical_layout_1.addWidget(credit_hour_spinner)
-	credit_hour_spinner.valueChanged.connect(filter_action)
+		self.credit_hours_check = QtWidgets.QCheckBox("Credit Hours", page)
+		vertical_layout_1.addWidget(self.credit_hours_check)
+		self.credit_hours_check.stateChanged.connect(self.filter_table)
 
-	title_label = QtWidgets.QLabel("Title", page)
-	vertical_layout_1.addWidget(title_label)
+		self.credit_hour_spinner = QtWidgets.QDoubleSpinBox(page)
+		self.credit_hour_spinner.setDecimals(1)
+		self.credit_hour_spinner.setSingleStep(0.5)
+		vertical_layout_1.addWidget(self.credit_hour_spinner)
+		self.credit_hour_spinner.valueChanged.connect(self.filter_table)
 
-	title_edit = QtWidgets.QLineEdit(page)
-	title_edit.setSizePolicy(size_policy)
-	vertical_layout_1.addWidget(title_edit)
-	title_edit.textChanged.connect(filter_action)
+		title_label = QtWidgets.QLabel("Title", page)
+		vertical_layout_1.addWidget(title_label)
 
-	instructor_label = QtWidgets.QLabel("Instructor", page)
-	vertical_layout_1.addWidget(instructor_label)
+		self.title_edit = QtWidgets.QLineEdit(page)
+		self.title_edit.setSizePolicy(size_policy)
+		vertical_layout_1.addWidget(self.title_edit)
+		self.title_edit.textChanged.connect(self.filter_table)
 
-	instructor_edit = QtWidgets.QLineEdit(page)
-	instructor_edit.setSizePolicy(size_policy)
-	vertical_layout_1.addWidget(instructor_edit)
-	instructor_edit.textChanged.connect(filter_action)
+		instructor_label = QtWidgets.QLabel("Instructor", page)
+		vertical_layout_1.addWidget(instructor_label)
 
-	lab_hours_check = QtWidgets.QCheckBox("Lab Hours", page)
-	vertical_layout_1.addWidget(lab_hours_check)
-	lab_hours_check.stateChanged.connect(filter_action)
+		self.instructor_edit = QtWidgets.QLineEdit(page)
+		self.instructor_edit.setSizePolicy(size_policy)
+		vertical_layout_1.addWidget(self.instructor_edit)
+		self.instructor_edit.textChanged.connect(self.filter_table)
 
-	lab_hours_spinner = QtWidgets.QDoubleSpinBox(page)
-	lab_hours_spinner.setDecimals(1)
-	lab_hours_spinner.setSingleStep(0.5)
-	vertical_layout_1.addWidget(lab_hours_spinner)
-	lab_hours_spinner.valueChanged.connect(filter_action)
+		self.lab_hours_check = QtWidgets.QCheckBox("Lab Hours", page)
+		vertical_layout_1.addWidget(self.lab_hours_check)
+		self.lab_hours_check.stateChanged.connect(self.filter_table)
 
-	lecture_hours_check = QtWidgets.QCheckBox("Lecture Hours", page)
-	vertical_layout_1.addWidget(lecture_hours_check)
-	lecture_hours_check.stateChanged.connect(filter_action)
+		self.lab_hours_spinner = QtWidgets.QDoubleSpinBox(page)
+		self.lab_hours_spinner.setDecimals(1)
+		self.lab_hours_spinner.setSingleStep(0.5)
+		vertical_layout_1.addWidget(self.lab_hours_spinner)
+		self.lab_hours_spinner.valueChanged.connect(self.filter_table)
 
-	lecture_hours_spinner = QtWidgets.QDoubleSpinBox(page)
-	lecture_hours_spinner.setDecimals(1)
-	lecture_hours_spinner.setSingleStep(0.5)
-	vertical_layout_1.addWidget(lecture_hours_spinner)
-	lecture_hours_spinner.valueChanged.connect(filter_action)
+		self.lecture_hours_check = QtWidgets.QCheckBox("Lecture Hours", page)
+		vertical_layout_1.addWidget(self.lecture_hours_check)
+		self.lecture_hours_check.stateChanged.connect(self.filter_table)
 
-	honors_check = QtWidgets.QCheckBox("Honors", page)
-	vertical_layout_1.addWidget(honors_check)
-	honors_check.stateChanged.connect(filter_action)
+		self.lecture_hours_spinner = QtWidgets.QDoubleSpinBox(page)
+		self.lecture_hours_spinner.setDecimals(1)
+		self.lecture_hours_spinner.setSingleStep(0.5)
+		vertical_layout_1.addWidget(self.lecture_hours_spinner)
+		self.lecture_hours_spinner.valueChanged.connect(self.filter_table)
 
-	description_label = QtWidgets.QLabel("Description", page)
-	vertical_layout_1.addWidget(description_label)
+		self.honors_check = QtWidgets.QCheckBox("Honors", page)
+		vertical_layout_1.addWidget(self.honors_check)
+		self.honors_check.stateChanged.connect(self.filter_table)
 
-	description_edit = QtWidgets.QTextEdit(page)
-	description_edit.setSizePolicy(size_policy)
-	description_edit.setMaximumSize(QtCore.QSize(120, 16777215))
-	vertical_layout_1.addWidget(description_edit)
-	description_edit.textChanged.connect(filter_action)
+		description_label = QtWidgets.QLabel("Description", page)
+		vertical_layout_1.addWidget(description_label)
 
-	spacer = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-	vertical_layout_1.addItem(spacer)
+		self.description_edit = QtWidgets.QTextEdit(page)
+		self.description_edit.setSizePolicy(size_policy)
+		self.description_edit.setMaximumSize(QtCore.QSize(120, 16777215))
+		vertical_layout_1.addWidget(self.description_edit)
+		self.description_edit.textChanged.connect(self.filter_table)
 
-	return_button = QtWidgets.QPushButton("Return to Menu", page)
-	vertical_layout_1.addWidget(return_button)
-	horizontal_layout_1.addLayout(vertical_layout_1)
-	return_button.clicked.connect(return_action)
+		spacer = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+		vertical_layout_1.addItem(spacer)
 
-	table = QtWidgets.QTableWidget(page)
-	size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-	size_policy.setHorizontalStretch(0)
-	size_policy.setVerticalStretch(0)
-	size_policy.setHeightForWidth(table.sizePolicy().hasHeightForWidth())
-	table.setSizePolicy(size_policy)
-	table.setSelectionMode(QtWidgets.QAbstractItemView.NoSelection)
-	table.setAlternatingRowColors(True)
-	table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
-	table.cellClicked.connect(show_course_action)
+		return_button = QtWidgets.QPushButton("Return to Menu", page)
+		vertical_layout_1.addWidget(return_button)
+		horizontal_layout_1.addLayout(vertical_layout_1)
+		return_button.clicked.connect(return_function)
 
-	horizontal_layout_1.addWidget(table)
-	grid_layout_1.addLayout(horizontal_layout_1, 0, 0, 1, 1)
-	stacked_widget.addWidget(page)
+		self.table = QtWidgets.QTableWidget(page)
+		size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+		size_policy.setHorizontalStretch(0)
+		size_policy.setVerticalStretch(0)
+		size_policy.setHeightForWidth(self.table.sizePolicy().hasHeightForWidth())
+		self.table.setSizePolicy(size_policy)
+		self.table.setSelectionMode(QtWidgets.QAbstractItemView.NoSelection)
+		self.table.setAlternatingRowColors(True)
+		self.table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+		self.table.cellClicked.connect(self.show_course)
 
-	return dept_edit, num_edit, title_edit, instructor_edit, description_edit, honors_check, table, credit_hours_check, credit_hour_spinner, lab_hours_check, lab_hours_spinner, lecture_hours_check, lecture_hours_spinner
+		horizontal_layout_1.addWidget(self.table)
+		grid_layout_1.addLayout(horizontal_layout_1, 0, 0, 1, 1)
+		stacked_widget.addWidget(page)
+
+	def populate_table(self):
+		self.table.setColumnCount(9)
+		self.table.setRowCount(len(self.data))
+		self.table.setHorizontalHeaderLabels(
+			["Dept", "Course Num", "Credits", "Title", "# Sections", "# Instructors", "Lab Hours", "Lecture Hours",
+			 "HIDDEN"])
+		for i, key in enumerate(self.data.keys()):
+			self.table.setItem(i, 0,
+			                   QtWidgets.QTableWidgetItem(Dao.none_safe(self.data[key]["dept_name"])))
+			self.table.setItem(i, 1, QtWidgets.QTableWidgetItem(
+				self.data[key]["catalog_number"] + Dao.none_safe(self.data[key]["catalog_suffix"])))
+			self.table.setItem(i, 2,
+			                   QtWidgets.QTableWidgetItem(Dao.none_safe(self.data[key]["credit_hours"])))
+			title = Dao.none_safe(self.data[key]["full_title"])
+			if title.endswith("."):
+				title = title[:-1]
+			self.table.setItem(i, 3, QtWidgets.QTableWidgetItem(title))
+			item = QtWidgets.QTableWidgetItem()
+			item.setData(QtCore.Qt.DisplayRole, len(self.data[key]["sections"]))
+			self.table.setItem(i, 4, item)
+			item = QtWidgets.QTableWidgetItem()
+			item.setData(QtCore.Qt.DisplayRole, len(self.data[key]["instructors"]))
+			self.table.setItem(i, 5, item)
+			lab_hours = Dao.none_safe(self.data[key]["lab_hours"])
+			try:
+				lab_hours = float(lab_hours)
+				item = QtWidgets.QTableWidgetItem()
+				item.setData(QtCore.Qt.DisplayRole, lab_hours)
+				self.table.setItem(i, 6, item)
+			except:
+				if "arr" in lab_hours.lower():
+					lab_hours = "variable"
+				self.table.setItem(i, 6, QtWidgets.QTableWidgetItem(lab_hours))
+			lecture_hours = Dao.none_safe(self.data[key]["lecture_hours"])
+			try:
+				lecture_hours = float(lecture_hours)
+				item = QtWidgets.QTableWidgetItem()
+				item.setData(QtCore.Qt.DisplayRole, lecture_hours)
+				self.table.setItem(i, 7, item)
+			except:
+				if "arr" in lecture_hours.lower():
+					lecture_hours = "variable"
+				self.table.setItem(i, 7, QtWidgets.QTableWidgetItem(lecture_hours))
+			self.table.setItem(i, 8,
+			                   QtWidgets.QTableWidgetItem(self.data[key]["curriculum_id_title_code"]))
+		self.table.hideColumn(8)
+		self.table.setSortingEnabled(True)
+		self.table.resizeColumnsToContents()
+
+	def filter_table(self):
+		dept_filter = self.dept_edit.text().lower()
+		num_filter = self.num_edit.text().lower()
+		credit_hours_checked = self.credit_hours_check.isChecked()
+		credit_hour_filter = self.credit_hour_spinner.value()
+		title_filter = self.title_edit.text().lower()
+		instructor_filter = self.instructor_edit.text().lower()
+		instructor_parts = instructor_filter.split(" ")
+		lab_hour_checked = self.lab_hours_check.isChecked()
+		lab_hour_filter = self.lab_hours_spinner.value()
+		lecture_hour_checked = self.lecture_hours_check.isChecked()
+		lecture_hours_filter = self.lecture_hours_spinner.value()
+		honors_checked = self.honors_check.isChecked()
+		description_filter = self.description_edit.toPlainText().lower()
+
+		for index in range(self.table.rowCount()):
+			show = True
+			try:
+				if dept_filter != "":
+					if dept_filter not in self.table.item(index, 0).text().lower():
+						show = False
+				if show and num_filter != "":
+					if num_filter not in self.table.item(index, 1).text().lower():
+						show = False
+				if show and credit_hours_checked:
+					if credit_hour_filter != float(self.table.item(index, 2).text()):
+						show = False
+				if show and title_filter != "":
+					if title_filter not in self.table.item(index, 3).text().lower():
+						show = False
+				if show and instructor_filter != "":
+					show = False
+					found = True
+					for instructor in self.data[self.table.item(index, 8).text()]["instructors"]:
+						if instructor is not None:
+							for part in instructor_parts:
+								if part not in instructor["sort_name"].lower():
+									found = False
+							if found:
+								show = True
+								break
+
+				if show and lab_hour_checked:
+					if lab_hour_filter != float(self.table.item(index, 6).text()):
+						show = False
+				if show and lecture_hour_checked:
+					if lecture_hours_filter != float(self.table.item(index, 7).text()):
+						show = False
+				if show and honors_checked and (
+						self.data[self.table.item(index, 8).text()]["honors_approved"] is None or
+						self.data[self.table.item(index, 8).text()]["honors_approved"].lower() == "n"):
+					show = False
+				if show and description_filter != "":
+					if self.data[self.table.item(index, 8).text()][
+						"description"] is None or description_filter not in \
+							self.data[self.table.item(index, 8).text()]["description"].lower():
+						show = False
+			except IOError as e:
+				print("error sorting")
+				print(e)
+				show = False
+
+			if show:
+				self.table.showRow(index)
+			else:
+				self.table.hideRow(index)
+
+	def show_course(self, row, column):
+		course_dialog = QtWidgets.QDialog()
+		course_ui = UI.dialog_course.Ui_Dialog()
+		course_ui.setupUi(course_dialog, self.data[self.table.item(row, 8).text()])
+		course_dialog.exec_()
+
+	def initialize_page(self):
+		self.dept_edit.clear()
+		self.num_edit.clear()
+		self.title_edit.clear()
+		self.instructor_edit.clear()
+		self.description_edit.clear()
+		self.honors_check.setChecked(False)
+		self.description_edit.clear()
--- UI/main_ui.py
+++ UI/main_ui.py
@@ -1,6 +1,5 @@
 from PyQt5 import QtCore, QtWidgets
 
-import Dao
 import UI.Pages.page_course
 import UI.Pages.page_instructor
 import UI.Pages.page_title
@@ -20,32 +19,11 @@ class Ui_MainWindow(object):
 			self.main_window_stacked_widget, self.browse_course_action, self.browse_section_action,
 			self.browse_instructor_action, self.make_schedule_action)
 
-		# items = UI.Pages.page_instructor.add_instructor_page(self.main_window_stacked_widget,
-		#                                                      self.filter_instructor_table,
-		#                                                      self.show_instructor,
-		#                                                      self.goto_title_page)
-		# self.instructor_first_name_input = items[0]
-		# self.instructor_last_name_input = items[1]
-		# self.instructor_course_input = items[2]
-		# self.instructor_table = items[3]
 		self.instructor_page = UI.Pages.page_instructor.InstructorPage(self.main_window_stacked_widget,
 		                                                               self.goto_title_page, self.loaded_data)
 
-		items = UI.Pages.page_course.add_course_page(self.main_window_stacked_widget, self.filter_course_table,
-		                                             self.show_course, self.goto_title_page)
-		self.course_dept_edit = items[0]
-		self.course_course_num_edit = items[1]
-		self.course_title_edit = items[2]
-		self.course_instructor_edit = items[3]
-		self.course_description_edit = items[4]
-		self.course_honors_check = items[5]
-		self.course_table = items[6]
-		self.course_credit_hours_check = items[7]
-		self.course_credit_hours_spinner = items[8]
-		self.course_lab_hours_check = items[9]
-		self.course_lab_hours_spinner = items[10]
-		self.course_lecture_hours_check = items[11]
-		self.course_lecture_hours_spinner = items[12]
+		self.course_page = UI.Pages.page_course.CoursePage(self.main_window_stacked_widget, self.goto_title_page,
+		                                                   self.loaded_data)
 
 		self.finish_setup_window()
 
@@ -78,14 +56,8 @@ class Ui_MainWindow(object):
 
 	def goto_browse_course(self) -> None:
 		self.main_window.resize(1350, 700)
-		self.course_dept_edit.clear()
-		self.course_course_num_edit.clear()
-		self.course_title_edit.clear()
-		self.course_instructor_edit.clear()
-		self.course_description_edit.clear()
-		self.course_honors_check.setChecked(False)
-		self.course_description_edit.clear()
-		self.populate_course_table()
+		self.course_page.initialize_page()
+		self.course_page.populate_table()
 		self.main_window_stacked_widget.setCurrentIndex(2)
 		self.main_window.setWindowTitle("Browse Courses")
 
@@ -125,126 +97,3 @@ class Ui_MainWindow(object):
 		                 self.loaded_data,
 		                 data_type)
 		popup_dialog.exec_()
-
-	def populate_course_table(self) -> None:
-		self.course_table.setColumnCount(9)
-		self.course_table.setRowCount(len(self.loaded_data))
-		self.course_table.setHorizontalHeaderLabels(
-			["Dept", "Course Num", "Credits", "Title", "# Sections", "# Instructors", "Lab Hours", "Lecture Hours",
-			 "HIDDEN"])
-		for i, key in enumerate(self.loaded_data.keys()):
-			self.course_table.setItem(i, 0,
-			                          QtWidgets.QTableWidgetItem(Dao.none_safe(self.loaded_data[key]["dept_name"])))
-			self.course_table.setItem(i, 1, QtWidgets.QTableWidgetItem(
-				self.loaded_data[key]["catalog_number"] + Dao.none_safe(self.loaded_data[key]["catalog_suffix"])))
-			self.course_table.setItem(i, 2,
-			                          QtWidgets.QTableWidgetItem(Dao.none_safe(self.loaded_data[key]["credit_hours"])))
-			title = Dao.none_safe(self.loaded_data[key]["full_title"])
-			if title.endswith("."):
-				title = title[:-1]
-			self.course_table.setItem(i, 3, QtWidgets.QTableWidgetItem(title))
-			item = QtWidgets.QTableWidgetItem()
-			item.setData(QtCore.Qt.DisplayRole, len(self.loaded_data[key]["sections"]))
-			self.course_table.setItem(i, 4, item)
-			item = QtWidgets.QTableWidgetItem()
-			item.setData(QtCore.Qt.DisplayRole, len(self.loaded_data[key]["instructors"]))
-			self.course_table.setItem(i, 5, item)
-			lab_hours = Dao.none_safe(self.loaded_data[key]["lab_hours"])
-			try:
-				lab_hours = float(lab_hours)
-				item = QtWidgets.QTableWidgetItem()
-				item.setData(QtCore.Qt.DisplayRole, lab_hours)
-				self.course_table.setItem(i, 6, item)
-			except:
-				if "arr" in lab_hours.lower():
-					lab_hours = "variable"
-				self.course_table.setItem(i, 6, QtWidgets.QTableWidgetItem(lab_hours))
-			lecture_hours = Dao.none_safe(self.loaded_data[key]["lecture_hours"])
-			try:
-				lecture_hours = float(lecture_hours)
-				item = QtWidgets.QTableWidgetItem()
-				item.setData(QtCore.Qt.DisplayRole, lecture_hours)
-				self.course_table.setItem(i, 7, item)
-			except:
-				if "arr" in lecture_hours.lower():
-					lecture_hours = "variable"
-				self.course_table.setItem(i, 7, QtWidgets.QTableWidgetItem(lecture_hours))
-			self.course_table.setItem(i, 8,
-			                          QtWidgets.QTableWidgetItem(self.loaded_data[key]["curriculum_id_title_code"]))
-		self.course_table.hideColumn(8)
-		self.course_table.setSortingEnabled(True)
-		self.course_table.resizeColumnsToContents()
-
-	# TODO put this in the class
-	def filter_course_table(self) -> None:
-		dept_filter = self.course_dept_edit.text().lower()
-		num_filter = self.course_course_num_edit.text().lower()
-		credit_hours_checked = self.course_credit_hours_check.isChecked()
-		credit_hour_filter = self.course_credit_hours_spinner.value()
-		title_filter = self.course_title_edit.text().lower()
-		instructor_filter = self.course_instructor_edit.text().lower()
-		instructor_parts = instructor_filter.split(" ")
-		lab_hour_checked = self.course_lab_hours_check.isChecked()
-		lab_hour_filter = self.course_lab_hours_spinner.value()
-		lecture_hour_checked = self.course_lecture_hours_check.isChecked()
-		lecture_hours_filter = self.course_lecture_hours_spinner.value()
-		honors_checked = self.course_honors_check.isChecked()
-		description_filter = self.course_description_edit.toPlainText().lower()
-
-		for index in range(self.course_table.rowCount()):
-			show = True
-			try:
-				if dept_filter != "":
-					if dept_filter not in self.course_table.item(index, 0).text().lower():
-						show = False
-				if show and num_filter != "":
-					if num_filter not in self.course_table.item(index, 1).text().lower():
-						show = False
-				if show and credit_hours_checked:
-					if credit_hour_filter != float(self.course_table.item(index, 2).text()):
-						show = False
-				if show and title_filter != "":
-					if title_filter not in self.course_table.item(index, 3).text().lower():
-						show = False
-				if show and instructor_filter != "":
-					show = False
-					found = True
-					for instructor in self.loaded_data[self.course_table.item(index, 8).text()]["instructors"]:
-						if instructor is not None:
-							for part in instructor_parts:
-								if part not in instructor["sort_name"].lower():
-									found = False
-							if found:
-								show = True
-								break
-
-				if show and lab_hour_checked:
-					if lab_hour_filter != float(self.course_table.item(index, 6).text()):
-						show = False
-				if show and lecture_hour_checked:
-					if lecture_hours_filter != float(self.course_table.item(index, 7).text()):
-						show = False
-				if show and honors_checked and (
-						self.loaded_data[self.course_table.item(index, 8).text()]["honors_approved"] is None or
-						self.loaded_data[self.course_table.item(index, 8).text()]["honors_approved"].lower() == "n"):
-					show = False
-				if show and description_filter != "":
-					if self.loaded_data[self.course_table.item(index, 8).text()][
-						"description"] is None or description_filter not in \
-							self.loaded_data[self.course_table.item(index, 8).text()]["description"].lower():
-						show = False
-			except IOError as e:
-				print("error sorting")
-				print(e)
-				show = False
-
-			if show:
-				self.course_table.showRow(index)
-			else:
-				self.course_table.hideRow(index)
-
-	def show_course(self, row: int, column: int) -> None:
-		course_dialog = QtWidgets.QDialog()
-		course_ui = UI.dialog_course.Ui_Dialog()
-		course_ui.setupUi(course_dialog, self.loaded_data[self.course_table.item(row, 8).text()])
-		course_dialog.exec_()
